19a106a3f910133462cb7b5a47815a57ab87d0bf
ZtereoHYPE <57519662+ZtereoHYPE@users.noreply.github.com>
2021-11-04 00:02:15 +0100

    change: rename networking shit

---
+++ src/main/java/codes/ztereohype/ztereomusic/networking/YoutubeSearch.java
@@ -0,0 +1,9 @@
+package codes.ztereohype.ztereomusic.networking;
+
+import java.io.IOException;
+
+public class YoutubeSearch {
+    public String getVideoUrl(String title) throws IOException {
+        return "WIP";
+    }
+}
