b3ba0db42efa437f3c3fdceb3cec08ee364f931a
ZtereoHYPE <57519662+ZtereoHYPE@users.noreply.github.com>
2022-02-03 03:06:19 +0100

    change: clean up and update config to json 0.3

--- src/main/java/codes/ztereohype/ztereomusic/database/Config.java
+++ src/main/java/codes/ztereohype/ztereomusic/database/Config.java
@@ -3,26 +3,24 @@ package codes.ztereohype.ztereomusic.database;
 import lombok.Getter;
 import net.shadew.json.Json;
 import net.shadew.json.JsonNode;
-import net.shadew.json.JsonSyntaxException;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Config {
+    private final static Json json5 = Json.json5();
     private @Getter final Map<String, String> propreties = new HashMap<>();
     private String path;
 
-    public static Config loadFrom(String path) throws JsonSyntaxException, FileNotFoundException {
+    public static Config loadFrom(String path) throws IOException {
         Config config = new Config();
 
-        Json json5 = Json.json5();
         JsonNode tree = json5.parse(new File(path));
-
         config.path = path;
 
-        for (String key : tree.keys()) {
+        for (String key : tree.keySet()) {
             config.getPropreties().put(key, tree.get(key).asString());
         }
 
